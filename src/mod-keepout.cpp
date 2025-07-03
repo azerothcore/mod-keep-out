5021f43e6c07595f28f20d0caa4f0cb9530cb339
Takenbacon <revoke1336@live.com>
2025-07-03 03:19:18 -0700

    fix(Core/Config): Use new config cache (#19)

--- src/mod-keepout.cpp
+++ src/mod-keepout.cpp
@@ -1,21 +1,40 @@
 #include "AccountMgr.h"
 #include "Chat.h"
 #include "Configuration/Config.h"
+#include "ConfigValueCache.h"
 #include "Creature.h"
 #include "Define.h"
 #include "GossipDef.h"
 #include "Player.h"
 #include "ScriptMgr.h"
 
-struct MKO
+enum class KeepOutConfig
 {
-    uint32 maxWarnings;
-    bool keepOutEnabled;
-    bool teleportEnabled;
-    bool kickEnabled;
+    MAX_WARNINGS,
+    ENABLED,
+    TELEPORT_ENABLED,
+    KICK_ENABLED,
+    ANNOUNCER_ENABLED,
+
+    NUM_CONFIGS,
 };
 
-MKO mko;
+class KeepOutConfigData : public ConfigValueCache<KeepOutConfig>
+{
+public:
+    KeepOutConfigData() : ConfigValueCache(KeepOutConfig::NUM_CONFIGS) { };
+
+    void BuildConfigCache() override
+    {
+        SetConfigValue<uint32>(KeepOutConfig::MAX_WARNINGS,     "MaxWarnings",              3);
+        SetConfigValue<bool>(KeepOutConfig::ENABLED,            "KeepOutEnabled",           true);
+        SetConfigValue<bool>(KeepOutConfig::TELEPORT_ENABLED,   "KeepOutTeleportEnabled",   true);
+        SetConfigValue<bool>(KeepOutConfig::KICK_ENABLED,       "KeepOutKickPlayerEnabled", true);
+        SetConfigValue<bool>(KeepOutConfig::ANNOUNCER_ENABLED,  "Announcer.Enable",         true);
+    }
+};
+
+static KeepOutConfigData keepOutConfigData;
 
 void teleportPlayer(Player* player)
 {
@@ -54,23 +73,23 @@ void checkZoneKeepOut(Player* player)
     {
         CharacterDatabase.Execute("INSERT INTO `mod_mko_map_exploit` (`accountId`, `count`) VALUES ({}, {})", accountId, countWarnings);
 
-        if (mko.teleportEnabled)
+        if (keepOutConfigData.GetConfigValue<bool>(KeepOutConfig::TELEPORT_ENABLED))
             teleportPlayer(player);
     }
     else
     {
         countWarnings = (*playerWarning)[1].Get<uint8>() + 1;
 
-        if (countWarnings <= mko.maxWarnings)
+        if (countWarnings <= keepOutConfigData.GetConfigValue<uint32>(KeepOutConfig::MAX_WARNINGS))
         {
             CharacterDatabase.Execute("UPDATE `mod_mko_map_exploit` SET `count`={} WHERE `accountId`={}", countWarnings, accountId);
             teleportPlayer(player);
         }
         else
         {
-            if (mko.teleportEnabled && !mko.kickEnabled)
+            if (keepOutConfigData.GetConfigValue<bool>(KeepOutConfig::TELEPORT_ENABLED) && !keepOutConfigData.GetConfigValue<bool>(KeepOutConfig::KICK_ENABLED))
                 teleportPlayer(player);
-            else if (mko.kickEnabled)
+            else if (keepOutConfigData.GetConfigValue<bool>(KeepOutConfig::KICK_ENABLED))
                 player->GetSession()->KickPlayer("MKO: Entering a place not allowed.", true);
             else
                 ChatHandler(player->GetSession()).PSendSysMessage("You have gone to a forbidden place your actions have been logged.");
@@ -88,13 +107,13 @@ public:
 
     void OnPlayerLogin(Player* player) override
     {
-        if (sConfigMgr->GetOption<bool>("Announcer.Enable", true))
+        if (keepOutConfigData.GetConfigValue<bool>(KeepOutConfig::ANNOUNCER_ENABLED))
             ChatHandler(player->GetSession()).PSendSysMessage("This server is running the |cff4CFF00Keep Out |rmodule.");
     }
 
     void OnPlayerUpdateZone(Player* player, uint32 /*newZone*/,  uint32 /*newArea*/) override
     {
-        if (mko.keepOutEnabled)
+        if (keepOutConfigData.GetConfigValue<bool>(KeepOutConfig::ENABLED))
             checkZoneKeepOut(player);
     }
 };
@@ -108,13 +127,7 @@ public:
 
     void OnBeforeConfigLoad(bool reload) override
     {
-        if (!reload)
-        {
-            mko.maxWarnings = sConfigMgr->GetOption<int>("MaxWarnings", 3);
-            mko.keepOutEnabled = sConfigMgr->GetOption<bool>("KeepOutEnabled", true);
-            mko.teleportEnabled = sConfigMgr->GetOption<bool>("KeepOutTeleportEnabled", true);
-            mko.kickEnabled = sConfigMgr->GetOption<bool>("KeepOutKickPlayerEnabled", true);
-        }
+        keepOutConfigData.Initialize(reload);
     }
 };
 
